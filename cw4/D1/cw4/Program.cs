4b86fd64d54366c995074d63f5ef94a4c102b3c4
Filip Cudny <filip96zx@gmail.com>
2020-04-08 14:46:43 +0200

    cw 4

---
+++ cw4/D1/cw4/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace cw4
+{
+    class Program
+    {
+        public class Osoba
+        {
+            public string imie, nazwisko;
+
+            public Osoba()
+            {
+                imie = "jan";
+                nazwisko = "kowalski";
+            }
+
+            public Osoba(string imie, string nazwisko)
+            {
+                this.imie = imie;
+                this.nazwisko = nazwisko;
+            }
+
+            public virtual void WypiszInfo()
+            {
+                Console.WriteLine(imie);
+                Console.WriteLine(nazwisko);
+            }
+        }
+
+        public class Student : Osoba
+        {
+            public string numerAlbumu, numerGrupy;
+            public int rokStudiow;
+
+            public Student()
+            {
+                imie = "jan";
+                nazwisko = "kowalski";
+                numerAlbumu = "1231";
+                numerGrupy = "1";
+                rokStudiow = 2;
+
+            }
+
+            public Student(string imie, string nazwisko, string numerAlbumu, string numerGrupy, int rokStudiow)
+            {
+                this.imie = imie;
+                this.nazwisko = nazwisko;
+                this.numerAlbumu = numerAlbumu;
+                this.numerGrupy = numerGrupy;
+                this.rokStudiow = rokStudiow;
+            }
+
+            public override void WypiszInfo()
+            {
+                base.WypiszInfo();
+                Console.WriteLine(numerAlbumu);
+                Console.WriteLine(numerGrupy);
+                Console.WriteLine(rokStudiow);
+
+            }
+
+            public override string ToString()
+            {
+                return imie + " " + nazwisko + " " + numerAlbumu + " " + numerGrupy + " " + rokStudiow;
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Osoba student1 = new Student();
+
+            student1.WypiszInfo();
+            Console.WriteLine(student1.ToString());
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ cw4/D3/D3/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace D3
+{
+    class Program
+    {
+        public abstract class Figura
+        {
+            protected double a, b, c;
+
+            public abstract double ObliczPole();
+
+        }
+
+        public class Kwadrat : Figura
+        {
+            public Kwadrat(int a)
+            {
+                this.a = a;
+            }
+
+            public override double ObliczPole()
+            {
+                return a * a;
+            }
+        }
+
+        public class Trojkat : Figura
+        {
+            public Trojkat(int a, int b, int c)
+            {
+                this.a = a;
+                this.b = b;
+                this.c = c;
+            }
+
+            public override double ObliczPole()
+            {
+                double p = (a + b + c) / 2;
+                return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<Figura> lista = new List<Figura>();
+
+            lista.Add(new Kwadrat(1));
+            lista.Add(new Kwadrat(2));
+            lista.Add(new Kwadrat(3));
+            lista.Add(new Trojkat(1, 3, 3));
+            lista.Add(new Trojkat(3, 3, 1));
+            lista.Add(new Trojkat(4, 5, 2));
+            lista.Add(new Trojkat(2, 2, 2));
+
+            foreach (Figura obj in lista)
+            {
+                Console.WriteLine("Pole figury: " + obj.ObliczPole());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
